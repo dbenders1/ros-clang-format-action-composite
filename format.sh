931e23a8dd8e383534c13c91ccc3758c0ba0ef7c
dbenders1 <dbenders@hotmail.nl>
2022-05-04 18:10:30 +0200

    Use formatting code in format.sh

--- format.sh
+++ format.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
-echo "format.sh"
+name=$INPUT_AUTHOR_NAME
+email=$INPUT_AUTHOR_EMAIL
+message=$INPUT_COMMIT_MESSAGE
 
-# name=$INPUT_AUTHOR_NAME
-# email=$INPUT_AUTHOR_EMAIL
-# message=$INPUT_COMMIT_MESSAGE
+apply_style(){
+  find . -name '*.h' -or -name '*.hpp' -or -name '*.cpp' | xargs clang-format-10 -i -style=file --verbose $1
+}
 
-# apply_style(){
-#   find . -name '*.h' -or -name '*.hpp' -or -name '*.cpp' | xargs clang-format-10 -i -style=file --verbose $1
-# }
+apt-get update
+apt-get install -y clang-format-10 git
 
-# apt-get update
-# apt-get install -y clang-format-10 git
+echo "======================="
+echo "Applying style to files"
+echo "======================="
+apply_style
 
-# echo "======================="
-# echo "Applying style to files"
-# echo "======================="
-# apply_style
+git config --global user.name "$name"
+git config --global user.email "$email"
+git config --global push.default current
 
-# git config --global user.name "$name"
-# git config --global user.email "$email"
-# git config --global push.default current
+modified_files=$(git status | grep modified)
 
-# modified_files=$(git status | grep modified)
-
-# if [[ $? == 0 ]] ;then
-#   echo $modified_files
-#   echo
-#   echo "============================"
-#   echo "Committing to Current Branch"
-#   echo "============================"
-#   git commit -a -m "$message"
-#   git push
-# else
-#   echo "No changes to commit"
-# fi
+if [[ $? == 0 ]] ;then
+  echo $modified_files
+  echo
+  echo "============================"
+  echo "Committing to Current Branch"
+  echo "============================"
+  git commit -a -m "$message"
+  git push
+else
+  echo "No changes to commit"
+fi
