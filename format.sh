450c919edf856db53fec80cf27ca2a9b86873fd0
dbenders1 <dbenders@hotmail.nl>
2022-05-04 18:05:41 +0200

    Update action

--- format.sh
+++ format.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-name=$INPUT_AUTHOR_NAME
-email=$INPUT_AUTHOR_EMAIL
-message=$INPUT_COMMIT_MESSAGE
+echo "format.sh"
 
-apply_style(){
-  find . -name '*.h' -or -name '*.hpp' -or -name '*.cpp' | xargs clang-format-10 -i -style=file --verbose $1
-}
+# name=$INPUT_AUTHOR_NAME
+# email=$INPUT_AUTHOR_EMAIL
+# message=$INPUT_COMMIT_MESSAGE
 
-apt-get update
-apt-get install -y clang-format-10 git
+# apply_style(){
+#   find . -name '*.h' -or -name '*.hpp' -or -name '*.cpp' | xargs clang-format-10 -i -style=file --verbose $1
+# }
 
-echo "======================="
-echo "Applying style to files"
-echo "======================="
-apply_style
+# apt-get update
+# apt-get install -y clang-format-10 git
 
-git config --global user.name "$name"
-git config --global user.email "$email"
-git config --global push.default current
+# echo "======================="
+# echo "Applying style to files"
+# echo "======================="
+# apply_style
 
-modified_files=$(git status | grep modified)
+# git config --global user.name "$name"
+# git config --global user.email "$email"
+# git config --global push.default current
 
-if [[ $? == 0 ]] ;then
-  echo $modified_files
-  echo
-  echo "============================"
-  echo "Committing to Current Branch"
-  echo "============================"
-  git commit -a -m "$message"
-  git push
-else
-  echo "No changes to commit"
-fi
+# modified_files=$(git status | grep modified)
+
+# if [[ $? == 0 ]] ;then
+#   echo $modified_files
+#   echo
+#   echo "============================"
+#   echo "Committing to Current Branch"
+#   echo "============================"
+#   git commit -a -m "$message"
+#   git push
+# else
+#   echo "No changes to commit"
+# fi
